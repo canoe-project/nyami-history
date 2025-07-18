3433828843c23905c62fcf91441a4da34ad6e649
canoe <ehgus430880@gmail.com>
2025-07-19 03:07:17 +0900

    Quartz sync: Jul 19, 2025, 3:07 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "냐미",
+    pageTitle: "냠동부 연구실",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
